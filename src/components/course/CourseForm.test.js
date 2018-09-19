54f4e4619d247e42d36c1b4e2dce094af9bdf740
lwpro2 <lwpro2@gmail.com>
2018-09-19 21:52:21 +0800

    react redux

--- src/components/course/CourseForm.test.js
+++ src/components/course/CourseForm.test.js
@@ -3,4 +3,26 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import CourseForm from './CourseForm';
 
-describe()
+function setup(){
+	let props = {
+		course: {}, saving: false, errors: {},
+		onSave: () => {},
+		onChange: () => {}
+	}
+	let renderer = TestUtils.createRenderer();
+	renderer.react(<CourseForm {...props} />);
+	let output = renderer.getRenderOutput();
+
+	return {
+		props,
+		output,
+		renderer
+	};
+}
+
+describe('CourseForm via React Test Utils', () =>{
+	it('renders form and h1', () => {
+			const {output} = setup();
+			expect(output.type).toBe('form');
+	});
+});
