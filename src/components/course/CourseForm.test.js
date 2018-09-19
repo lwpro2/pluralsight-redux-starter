e27c32826eb6a2ab66479719c3e380cede2f3c2d
lwpro2 <lwpro2@gmail.com>
2018-09-19 22:00:14 +0800

    react redux

--- src/components/course/CourseForm.test.js
+++ src/components/course/CourseForm.test.js
@@ -3,14 +3,14 @@ import React from 'react';
 import TestUtils from 'react-addons-test-utils';
 import CourseForm from './CourseForm';
 
-function setup(){
+function setup(saving){
 	let props = {
-		course: {}, saving: false, errors: {},
+		course: {}, saving: saving, errors: {},
 		onSave: () => {},
 		onChange: () => {}
 	}
 	let renderer = TestUtils.createRenderer();
-	renderer.react(<CourseForm {...props} />);
+	renderer.render(<CourseForm {...props} />);
 	let output = renderer.getRenderOutput();
 
 	return {
@@ -24,5 +24,27 @@ describe('CourseForm via React Test Utils', () =>{
 	it('renders form and h1', () => {
 			const {output} = setup();
 			expect(output.type).toBe('form');
+
+			let [h1] = output.props.children;
+			expect(h1.type).toBe('h1');
 	});
+
+	it('save button is labeled "Save" when not saving', () => {
+			const {output} = setup(false);
+			const submitButton = output.props.children[5];
+			expect(submitButton.props.value).toBe('Save');
+
+			const {props} = setup(false);
+			expect(props.saving).toBe(false);
+
+	});
+
+	it('save button is labeled "Saving..." when saving', () => {
+			const {output} = setup(true);
+			const submitButton = output.props.children[5];
+			expect(submitButton.props.value).toBe('Saving...');
+
+	});
+
+
 });
